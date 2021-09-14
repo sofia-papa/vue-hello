47df6b958f6fe7736545c8aa2c6cc63de0b64aa6
sofia-papa <papa.sofia@outlook.it>
2021-09-14 18:01:23 +0100

    image

--- js/myscript.js
+++ js/myscript.js
@@ -3,7 +3,8 @@ let app = new Vue (
 {
     el: "#my_container",
     data: {
-        title: "Benvenuti in Boolean"
+        title: "Benvenuti in Boolean",
+        image: "https://www.industrialempathy.com/img/remote/ZiClJf-1280w.webp"
     }
 
 });
